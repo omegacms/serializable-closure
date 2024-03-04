197461c4159271af83207b1e695be39204f15cef
omegacms <omegacms@outlook.com>
2024-03-04 21:07:24 +0100

    Added support for serialization of anonymous functions

--- src/SerializableClosure.php
+++ src/SerializableClosure.php
@@ -4,7 +4,7 @@
  *
  * @link       https://omegacms.github.io
  * @author     Adriano Giovannini <omegacms@outlook.com>
- * @copyright  Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.io)
+ * @copyright  Copyright (c) 2022 Adriano Giovanni. (https://omegacms.github.io)
  * @license    https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
  */
 
@@ -23,12 +23,17 @@ namespace Omega\SerializableClosure;
  */
 use function call_user_func_array;
 use function func_get_args;
+use function get_class;
+use function is_object;
+use function serialize;
 use Closure;
-use Omega\SerializableClosure\Exceptions\InvalidSignatureException;
 use Omega\SerializableClosure\Serializers\Native;
 use Omega\SerializableClosure\Serializers\Signed;
 use Omega\SerializableClosure\Serializers\SerializableInterface;
 use Omega\SerializableClosure\Signers\Hmac;
+use ReflectionClass;
+use ReflectionException;
+use ReflectionFunction;
 
 /**
  * Serializable closure class.
@@ -42,7 +47,7 @@ use Omega\SerializableClosure\Signers\Hmac;
  * @package     Omega\SerializableClosure
  * @link        https://omegacms.github.io
  * @author      Adriano Giovannini <omegacms@outlook.com>
- * @copyright   Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.io)
+ * @copyright   Copyright (c) 2022 Adriano Giovanni. (https://omegacms.github.io)
  * @license     https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
  * @version     1.0.0
  */
@@ -81,7 +86,7 @@ class SerializableClosure
     /**
      * Gets the closure.
      *
-     * @return Closure Return the current closure objecy.
+     * @return Closure Return the current closure object.
      */
     public function getClosure() : Closure
     {
@@ -137,12 +142,27 @@ class SerializableClosure
     /**
      * Get the serializable representation of the closure.
      *
-     * @return array Return an array of the serialized repreentation of the closure.
+     * @return array Return an array of the serialized representation of the closure.
+     * @throws ReflectionException
      */
     public function __serialize() : array
     {
+        $closure = $this->serializable->getClosure();
+
+        // Check if the closure contains an anonymous class
+        $reflectionFunction = new ReflectionFunction( $closure );
+        $uses               = $reflectionFunction->getStaticVariables();
+
+        foreach ( $uses as $variable => $value ) {
+            if ( is_object( $value ) && $this->isAnonymousClass( $value ) ) {
+                // Handle anonymous class serialization
+                $uses[ $variable ] = $this->serializeAnonymousClass( $value );
+            }
+        }
+
         return [
             'serializable' => $this->serializable,
+            'uses'         => $uses,
         ];
     }
 
@@ -151,14 +171,81 @@ class SerializableClosure
      *
      * @param  array $data Holds an array of the closure data for restore.
      * @return void
-     * @throws InvalidSignatureException if the signature is not valid.
+     * @throws ReflectionException
      */
     public function __unserialize( array $data ) : void
     {
-        if ( Signed::$signer && ! $data[ 'serializable' ] instanceof Signed ) {
-            throw new InvalidSignatureException();
+        $this->serializable = $data[ 'serializable' ];
+
+        // Handle anonymous class deserialization
+        $uses = $data[ 'uses' ];
+
+        foreach ( $uses as $variable => $value ) {
+            if ( is_array( $value ) && $value[ '__anonymous_class' ] ?? false ) {
+                // Restore the anonymous class instance
+                $uses[ $variable ] = $this->unserializeAnonymousClass( $value );
+            }
         }
 
-        $this->serializable = $data[ 'serializable' ];
+        // Set the static variables of the closure
+        $this->setClosureUses( $uses );
+    }
+
+    /**
+     * Check if an object is an anonymous class.
+     *
+     * @param  object $object Holds the class object to check.
+     * @return bool Return true if the object is anonymous class, false if not.
+     */
+    protected function isAnonymousClass( object $object ) : bool
+    {
+        return ( new ReflectionClass( $object ) )->isAnonymous();
+    }
+
+    /**
+     * Serialize a anonymous class.
+     *
+     * @param  object $object Holds the anonymous class to serialize.
+     * @return array Return an array of serialize anonymous class.
+     */
+    protected function serializeAnonymousClass( object $object ) : array
+    {
+        // Customize the serialization of the anonymous class as needed
+        return [
+            '__anonymous_class' => true,
+            '__class_name'      => get_class( $object ),
+            '__class_data'      => serialize( $object ),
+        ];
+    }
+
+    /**
+     * Unserialize an anonymous class.
+     *
+     * @param  array $data Holds an array of anonymous class to unserialize.
+     * @return object Return the unserialized object class for class.
+     */
+    protected function unserializeAnonymousClass( array $data ) : object
+    {
+        // Customize the unserialization of the anonymous class as needed.
+        return unserialize( $data[ '__class_data' ] );
+    }
+
+    /**
+     * Set the static variables of the closure.
+     *
+     * @param  array $uses Holds an array of use variables associated with the closure.
+     * @return void
+     * @throws ReflectionException
+     */
+    protected function setClosureUses( array $uses ) : void
+    {
+        $reflectionFunction = new ReflectionFunction( $this->serializable->getClosure() );
+
+        // Use ReflectionFunction to set the static variables.
+        $closureThis = $reflectionFunction->getClosureThis();
+
+        if ( $closureThis !== null ) {
+            $closureThis->uses = $uses;
+        }
     }
 }
